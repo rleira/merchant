33dad913d5f86d7909da9208f5673ab58542573b
Rodrigo Leira <rodrigoleira@gmail.com>
2018-11-11 14:35:52 -0300

    Adding seeds

--- src/main/java/com/dlocal/merchant/MerchantApplication.java
+++ src/main/java/com/dlocal/merchant/MerchantApplication.java
@@ -1,5 +1,7 @@
 package com.dlocal.merchant;
 
+import com.mongodb.Mongo;
+import org.mongeez.Mongeez;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
@@ -7,6 +9,7 @@ import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
@@ -19,6 +22,16 @@ public class MerchantApplication extends SpringBootServletInitializer {
 		factory.setContextPath("/api");
 		factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
 		return factory;
+	}
+
+	@Bean(initMethod = "process")
+	public Mongeez loadInitialData() {
+		Mongeez mongeez = new Mongeez();
+		mongeez.setFile(new ClassPathResource("/mongeez.xml"));
+		mongeez.setMongo(new Mongo("localhost", 27017));
+		mongeez.setDbName("merchantDB");
+		mongeez.process();
+		return mongeez;
 	}
 
 	public static void main(String[] args) {
